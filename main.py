1fd5c7ad1560c165b4f9e63cefb7636ebd0177f8
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-18 10:34:31 -0400

    Added stuff?

--- main.py
+++ main.py
@@ -9,12 +9,30 @@ jinja_env = jinja2.Environment(
 
 class HtmlHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('TestHandler')
-        template= jinja_env.get_template('solution/templates/fortune_welcome.html')
+         #self.response.write('TestHandler')
+        template= jinja_env.get_template('index.html')
         testvariable= {
             "list_name" : ["1", "2", "3", "4", "5"],
         }
         self.response.write(template.render(testvariable))
+
+class BlogHandler(webapp2.RequestHandler):
+    def get(self):
+        template= jinja_env.get_template('templates/new_post.html')
+        self.response.write(template.render())
+class BlogHandler2(webapp2.RequestHandler):
+    def post(self):
+        variable= jinja_env.get_template('index.html')
+        blogvar= {
+        "title_var" : self.request.get('title'),
+        "content_var" : self.request.get('content'),
+        "author_var" : self.request.get('author'),
+        }
+        template = jinja_env.get_template('templates/view_post.html')
+        self.response.write(template.render(blogvar))
+
 app = webapp2.WSGIApplication([
-        ('/', HtmlHandler)
+        ('/', HtmlHandler),
+        ('/submit', BlogHandler),
+        ('/submitted', BlogHandler2),
 ], debug=True)
